9069f11a1b7c2e4b9c7fcb1ac5b4cd57b0334592
Alexandre <alexandrebrosteau@gmail.com>
2023-07-28 15:47:30 +0200

    match DTO

--- src/main/java/com/mycompany/tennis/App.java
+++ src/main/java/com/mycompany/tennis/App.java
@@ -1,9 +1,6 @@
 package com.mycompany.tennis;
 
-import com.mycompany.tennis.controller.EpreuveController;
-import com.mycompany.tennis.controller.JoueurController;
 import com.mycompany.tennis.controller.ScoreController;
-import com.mycompany.tennis.controller.TournoiController;
 
 /**
  * Hello world!
@@ -13,18 +10,8 @@ public class App
 {
     public static void main( String[] args )
     {
-
-       /* JoueurController joueurController = new JoueurController();
-        joueurController.displayPlayer();*/
-
-       /* TournoiController tournoiController = new TournoiController();
-        tournoiController.getTournoi();*/
-
-        /*JoueurController joueurController = new JoueurController();
-        joueurController.createPlayer();*/
-
-        EpreuveController controller = new EpreuveController();
-        controller.afficheDernierEpreuve();
+        ScoreController controller = new ScoreController();
+        controller.scoreById();
 
     }
 }
--- src/main/java/com/mycompany/tennis/controller/ScoreController.java
+++ src/main/java/com/mycompany/tennis/controller/ScoreController.java
@@ -1,5 +1,6 @@
 package com.mycompany.tennis.controller;
 
+import com.mycompany.tennis.core.dto.ScoreFullDto;
 import com.mycompany.tennis.core.entity.Score;
 import com.mycompany.tennis.core.service.ScoreService;
 
@@ -17,7 +18,7 @@ public class ScoreController {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Quel est l'identifiant du score à récupérer?");
         long id = scanner.nextLong();
-        Score score = scoreService.getScore(id);
+        ScoreFullDto score = scoreService.getScore(id);
 
         System.out.println("Les sets sont du score " + score.getId());
         System.out.println("SET 1 : " + score.getSet1());
@@ -25,5 +26,12 @@ public class ScoreController {
         if (score.getSet3() != null)System.out.println("SET 3 : " + score.getSet3());
         if (score.getSet4() != null)System.out.println("SET 4 : " + score.getSet4());
         if (score.getSet5() != null)System.out.println("SET 5 : " + score.getSet5());
+
+        System.out.println("Tournament name : " + score.getMatch().getEpreuve().getTournoi().getNom() + " passé en " + score.getMatch().getEpreuve().getAnnee());
+        System.out.println("Il s'agissait d'une épreuve de type " + (score.getMatch().getEpreuve().getType().charValue() == 'H' ? "Homme" : "Femme"));
+        System.out.println("Le vainqueur du match est : " + score.getMatch().getVainqueur().getNom() + score.getMatch().getVainqueur().getPrenom());
+        System.out.println("Le finaliste du match est : " + score.getMatch().getFinaliste().getNom() + score.getMatch().getFinaliste().getPrenom());
+
+
     }
 }
